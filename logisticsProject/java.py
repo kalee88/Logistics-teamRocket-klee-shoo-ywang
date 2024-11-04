cb4dfc0cd2c42cb1704223c407f12e12ce238820
Stanley Hoo <129146846+Stanleyhoo1@users.noreply.github.com>
2024-11-04 13:15:00 -0500

    Update java.py

--- logisticsProject/java.py
+++ logisticsProject/java.py
@@ -2,9 +2,19 @@ Web VPython 3.2
 
 class Imaginary:
     def __init__(self, real, complex, power=1):
-        self.real = real
-        self.complex = complex
-        self.power = power%4
+        self.power = power%2
+        if power%4==1:
+            self.real = real
+            self.complex = complex
+        elif power%4==2:
+            self.real = real-complex
+            self.complex = 0
+        elif power%4==3:
+            self.real = real
+            self.complex = -complex
+        else:
+            self.real = real+complex
+            self.complex = 0
         
     def set_real(self, real):
         self.real = real
@@ -25,8 +35,10 @@ class Imaginary:
         return self.power
         
     def display(self):
-        return f'{str(self.real)} + {str(self.complex)}i'
+        operation_sign = '+' if self.complex >=0 else '-'
+        return f'{str(self.real)} {operation_sign} {str(abs(self.complex))}i'
         
+# Adds complex numbers
 def add_complex(complex_list):
     real = 0
     complex = 0
@@ -44,6 +56,7 @@ def add_complex(complex_list):
             complex -= imag
     return Imaginary(real, complex)
 
+# Subtracts complex numbers
 def subtract_complex(complex_list):
     new_list = []
     new_list.append(complex_list[0])
@@ -53,8 +66,25 @@ def subtract_complex(complex_list):
         new_list.append(num)
     return add_complex(new_list)
     
+# Multiply complex numbers
+def mult_complex(complex_list):
+    real1, complex1, p1 = complex_list[0].get_real(), complex_list[0].get_imag(), complex_list[0].get_power()
+    
+    for num in complex_list[1:]:
+        real2, complex2, p2 = num.get_real(), num.get_imag(), num.get_power()
+        bd = -(complex1 * complex2)
+        new_real = real1 * real2 + bd
+        ad = real1 * complex2
+        bc = real2 * complex1
+        new_imag = ad + bc
+        
+        real1, complex1, p1 = new_real, new_imag, 1
+    return Imaginary(real1, complex1, p1)
+    
 a = Imaginary(3, 4, 1)
-b = Imaginary(7, 2, 6)
-result = subtract_complex([a, b])
+b = Imaginary(7, 2, 3)
+c = Imaginary(8, 5, 9)
+d = Imaginary(3, 8, 6)
+e = Imaginary(6, 3, 2)
+result = mult_complex([a, b, c, d, e])
 print(result.display())
-
